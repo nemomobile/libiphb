9e3fba174568661a9ba4a30f418afbe697755687
Matti Kosola <matti.kosola@jollamobile.com>
2013-05-24 16:22:24 +0300

    Add rpm directory and needed files

---
+++ verify_version.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Check that all files that should have the current version agree on it
+
+AC_PATH=configure.ac
+AC_VERS=$(sed '/AC_INIT/!d; s/AC_INIT(libiphb, \(.*\))/\1/' $AC_PATH)
+
+RPM_PATH=rpm/libiphb.spec
+RPM_VERS=$(grep '^Version:' $RPM_PATH |sed -e 's/^.*:[[:space:]]*//')
+
+YAML_PATH=rpm/libiphb.yaml
+YAML_VERS=$(grep '^Version:' $YAML_PATH |sed -e 's/^.*:[[:space:]]*//')
+
+RES=0
+
+if [ "$RPM_VERS" != "$AC_VERS" ]; then
+  echo >&2 "$AC_PATH $AC_VERS vs $RPM_PATH $RPM_VERS"
+  RES=1
+fi
+
+if [ "$RPM_VERS" != "$YAML_VERS" ]; then
+  echo >&2 "$YAML_PATH $YAML_VERS vs $RPM_PATH $RPM_VERS"
+  RES=1
+fi
+
+if [ $RES != 0 ]; then
+  echo >&2 "Conflicting package versions"
+fi
+
+exit $RES
